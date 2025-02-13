93d6497351a7ad4363bb8d149b1ca02cf23a6d6f
rafael-gino2 <rafaelgino27@outlook.com>
2025-02-12 22:34:18 -0300

    exercicio

---
+++ exercicio.py
@@ -0,0 +1,9 @@
+#Calculador de média de notas
+#Int = Numero inteiro, Float = Numeros inteiros e nn inteiros
+
+notaUm = input('Nota um:')
+notaDois = input('Nota dois:')
+notaTres = input('Nota três:')
+notaQuatro = input('Nota Quatro:')
+resultado = (int (notaUm) + int(notaDois) + int(notaTres) + int(notaQuatro)) / 4
+print(resultado)
